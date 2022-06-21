365c42971d41c64d3c77d7c4d1bd742b2d3c3fee
stanluu <42020235+stanluu@users.noreply.github.com>
2022-06-20 21:45:56 -0400

    update

--- test-merge/src/App.js
+++ test-merge/src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "@types/react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="col">
+        <h5>About</h5>
+        <ul>
+          <li>
+            <Link to="/">Learn about</Link>
+          </li>
+          <li>
+            <Link to="/">Agents</Link>
+          </li>
+          <li>
+            <Link to="/">Careers</Link>
+          </li>
+          <li>
+            <Link to="/">Get the inside story</Link>
+          </li>
+          <li>
+            <Link to="/">Press</Link>
+          </li>
+          <li>
+            <Link to="/">Communities</Link>
+          </li>
+          <li>
+            <Link to="/">Store</Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
---
+++ test-merge/src/test/asd.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h5>AAAA</h5>
+            <p>
+              How To Make Animated Website Footer Design Using HTML And CSS Step
+              by Step Tutorial | Website Footer design in 15 minutes
+            </p>
+            <input type="text" />
+            <button id="subscribe" type="submit">
+              Subscribe
+            </button>
+          </div>
+          <div className="col">
+            <h5>About</h5>
+            <ul>
+              <li>
+                <Link to="/">Learn about</Link>
+              </li>
+              <li>
+                <Link to="/">Agents</Link>
+              </li>
+              <li>
+                <Link to="/">Careers</Link>
+              </li>
+              <li>
+                <Link to="/">Get the inside story</Link>
+              </li>
+              <li>
+                <Link to="/">Press</Link>
+              </li>
+              <li>
+                <Link to="/">Communities</Link>
+              </li>
+              <li>
+                <Link to="/">Store</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="col">
+            <h5>Support</h5>
+            <ul>
+              <li>
+                <Link to="/">Resources</Link>
+              </li>
+              <li>
+                <Link to="/">Help</Link>
+              </li>
+              <li>
+                <Link to="/">Safety information</Link>
+              </li>
+              <li>
+                <Link to="/">Supporting People</Link>
+              </li>
+              <li>
+                <Link to="/">Cancellation options</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="col">
+            <h5>Support</h5>
+            <ul>
+              <li>
+                <Link to="/">Resources</Link>
+              </li>
+              <li>
+                <Link to="/">Help Center</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="col">
+            <h5>Support</h5>
+            <ul>
+              <li>
+                <Link to="/">Resources</Link>
+              </li>
+              <li>
+                <Link to="/">Help Center</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
